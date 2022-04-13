a3fd0116861ed772f11c65a5d399e7038fa37a8c
Caleb Goddard <goddard.103@osu.edu>
2022-04-13 00:16:54 -0400

    Set width of calendar smaller for mobile

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import React from 'react';
 function App() {
 
   return (
-    <div className="App">
+    <div className="App" height='100%'>
       <header> Concerts that I kind of want to go to </header>
       <br></br>
       <div>
-        <iframe title="Concert Calendar" src="https://calendar.google.com/calendar/embed?height=400&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&mode=AGENDA&showTz=0&showCalendars=0&showTabs=0&showPrint=0&showNav=0&showDate=1&showTitle=0&src=ZWgwZWNnMnBkajY0b3JvZW01ZTVuNWxrdGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23616161" styles="border:solid 1px #777" width="500" height="400" frameborder="0" scrolling="no"></iframe>
+        <iframe title="Concert Calendar" src="https://calendar.google.com/calendar/embed?height=400&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&mode=AGENDA&showTz=0&showCalendars=0&showTabs=0&showPrint=0&showNav=0&showDate=1&showTitle=0&src=ZWgwZWNnMnBkajY0b3JvZW01ZTVuNWxrdGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23616161" styles="border:solid 1px #777" width="320" height="400" frameborder="0" scrolling="no"></iframe>
       </div>
     </div>
   );
