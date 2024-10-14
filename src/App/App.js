ecc3f9158a05930f59bc18869bdeedc0f7e5fb47
calgod <thecalebeffect@gmail.com>
2024-10-14 01:34:03 -0400

    Added new EventTable component to replace google calendar iframe.

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { Link } from "react-router-dom";
+import EventTable from '../EventTable/EventTable';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -11,11 +12,11 @@ function App() {
   return (
     <div className='App'>
       <div className='headerDiv'>
-        <h1 className='header'>Concerts that I kind of want to go to</h1>
+        <h1 className='pageHeader'>Concerts that I kind of want to go to</h1>
       </div>
       <br></br>
       <div>
-        <iframe title="Concert Calendar" src="https://calendar.google.com/calendar/embed?height=400&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&mode=AGENDA&showTz=0&showCalendars=0&showTabs=0&showPrint=0&showNav=0&showDate=1&showTitle=0&src=ZWgwZWNnMnBkajY0b3JvZW01ZTVuNWxrdGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23616161" styles="border:solid 1px #777" width="320" height="400" frameborder="0" scrolling="no"></iframe>
+        <EventTable></EventTable>
       </div>
       <br></br>
       <div>
