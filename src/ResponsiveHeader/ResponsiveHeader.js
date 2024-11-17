7d2dce59e603927a206a17791eb10892436aa136
calgod <thecalebeffect@gmail.com>
2024-11-17 12:45:46 -0500

    Added ResponsiveHeader component to display SVG header based on max screen width

---
+++ src/ResponsiveHeader/ResponsiveHeader.js
@@ -0,0 +1,30 @@
+import { Box } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+import desktopLogo from '../assets/cals_concert_calendar_lettering.svg';
+import mobileLogo from '../assets/cals_concert_calendar_lettering_mobile.svg';
+
+function ResponsiveHeader() {
+  const isMobile = useMediaQuery('(max-width: 768px)');
+
+  return (
+    <Box 
+        sx={(theme) => ({
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '2rem',
+        paddingTop: isMobile ? '100px' : '2rem'
+        })}
+    >
+      <img 
+        src={isMobile ? mobileLogo : desktopLogo}
+        alt="Concert Calendar"
+        style={{
+          width: '100%',
+          maxWidth: isMobile ? '400px' : '900px'
+        }}
+      />
+    </Box>
+  );
+}
+
+export default ResponsiveHeader;
