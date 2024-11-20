7faae4b356744bce869dfb28325c44f20ef24722
calgod <thecalebeffect@gmail.com>
2024-11-20 15:40:59 -0500

    removed styles

--- src/ConcertHistory/ConcertHistory.js
+++ src/ConcertHistory/ConcertHistory.js
@@ -3,13 +3,13 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { Card, Text, Group, Collapse, SimpleGrid } from '@mantine/core';
 
 function ConcertHistory() {
 
   return (
-    <div className='App'>
-        concert history page (work in progress)
-    </div>
+    <Text> concert history page (work in progress) </Text>
+
   );
 }
 
