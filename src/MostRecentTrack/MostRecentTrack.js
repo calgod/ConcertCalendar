ff509836f61d6e7c1f309d8a55ffe98e4bd01e27
calgod <thecalebeffect@gmail.com>
2025-01-26 18:05:07 -0500

    removed username and apikey, updated padding

--- src/MostRecentTrack/MostRecentTrack.js
+++ src/MostRecentTrack/MostRecentTrack.js
@@ -3,7 +3,7 @@ import { Card, Group, Image, Text } from '@mantine/core';
 import './MostRecentTrack.css'
 import { fetchLastFm } from '../API/api';
 
-const MostRecentTrack = ({ username, apiKey }) => {
+const MostRecentTrack = () => {
   const [track, setTrack] = useState(null);
   const [error, setError] = useState(null);
 
@@ -21,33 +21,35 @@ const MostRecentTrack = ({ username, apiKey }) => {
     };
 
     fetchRecentTrack();
-  }, [username, apiKey]);
+  }, []);
 
   if (error) {
     return <div>{error}</div>;
   }
 
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      {track ? (
-        <Group noWrap>
-          <Image
-            src={track.image[2]['#text']}
-            alt="Album Art"
-            width={100}
-            height={100}
-            radius="md"
-          />
-          <div className='textDiv'>
-            <Text weight={500}><strong>Track:</strong> {track.name}</Text>
-            <Text><strong>Artist:</strong> {track.artist['#text']}</Text>
-            {track.album['#text'] && <Text><strong>Album:</strong> {track.album['#text']}</Text>}
-          </div>
-        </Group>
-      ) : (
-        <Text>Loading...</Text>
-      )}
-    </Card>
+    <div className='cardDiv'>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        {track ? (
+          <Group noWrap>
+            <Image
+              src={track.image[2]['#text']}
+              alt="Album Art"
+              width={100}
+              height={100}
+              radius="md"
+            />
+            <div className='textDiv'>
+              <Text weight={500}><strong>Track:</strong> {track.name}</Text>
+              <Text><strong>Artist:</strong> {track.artist['#text']}</Text>
+              {track.album['#text'] && <Text><strong>Album:</strong> {track.album['#text']}</Text>}
+            </div>
+          </Group>
+        ) : (
+          <Text>Loading...</Text>
+        )}
+      </Card>
+    </div>
   );
 };
 
