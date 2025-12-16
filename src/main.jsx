8407ff9084fbb878aa2ff36c379e5de7de1cfdb1
calgod <thecalebeffect@gmail.com>
2025-12-16 18:18:27 -0500

    Install and initialize Tanstack Query

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,32 @@ import './main.css';
 import App from './App/App';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <MantineProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </MantineProvider>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<App />} />
+          </Routes>
+        </BrowserRouter>
+
+        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+      </MantineProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
