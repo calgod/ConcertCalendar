648f52357cbfe1324d84d45266fbba7f17712692
calgod <thecalebeffect@gmail.com>
2025-12-20 16:53:28 -0500

    API -> api

--- src/hooks/useLastFm.js
+++ src/hooks/useLastFm.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchLastFm } from '../API/api';
+import { fetchLastFm } from '../api/api';
 
 export function useLastFm() {
   return useQuery({
--- src/hooks/useSheetEvents.js
+++ src/hooks/useSheetEvents.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchCalendarEvents } from '../API/api';
+import { fetchCalendarEvents } from '../api/api';
 
 export function useSheetEvents() {
   return useQuery({
